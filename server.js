72945d128a2998916614e34eee8a069a20304391
aa055 <aamirmoiz531@outlook.com>
2024-12-17 13:27:00 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import reviews from "./api/reviews.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/reviews", reviews)
+app.use("*", (req, res) => res.status(404).json({error: "not not found"}))
+
+export default app
+
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, world!');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
